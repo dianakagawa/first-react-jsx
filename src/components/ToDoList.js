9fcaabc13c5227145caf0e0160af8c0b746ba164
Diana Nakagawa <drcnakagawa@gmail.com>
2023-01-08 22:57:09 -0300

    first commit

---
+++ src/components/ToDoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function ToDo() {
+  return (
+    <ul>
+      <li> Learn React</li>
+      <li> Climb Mt. Everest</li>
+      <li> Run a marathon</li>
+      <li> Feed the doggo</li>
+    </ul>
+  );
+}
